febe2514f63ed24e3a97a32200ffdf259e380e61
madninja4 <cmcreedon@bsu.edu>
2020-02-13 11:20:43 -0500

    Implemented Test Class

--- test/InsuranceCalculatorTest.java
+++ test/InsuranceCalculatorTest.java
@@ -1,7 +1,30 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class InsuranceCalculatorTest {
+//Created Test class @ 11:19AM 2/13/2020
+public class InsuranceCalculatorTest {
+    @Test
+    public void low() {
+        assertEquals(1825, insuranceFor(5000), 0.01);
+    }
 
+    @Test
+    public void medium() {
+        assertEquals(38600, insuranceFor(25000), 0.01);
+    }
+
+    @Test
+    public void high() {
+        assertEquals(78500, insuranceFor(50000), 0.01);
+    }
+
+    @Test
+    public void veryHigh() {
+        assertEquals(106400, insuranceFor(100000), 0.01);
+    }
+
+    private double insuranceFor(double income) {
+        return new InsuranceCalculator().calculateInsurance(income);
+    }
 }
