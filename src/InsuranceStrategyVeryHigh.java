e5e45702fbfbdd1243b3b241b7060125a01af1cc
madninja4 <cmcreedon@bsu.edu>
2020-02-13 11:51:22 -0500

    Refactored InsuranceCalculator to have superclass

--- src/InsuranceStrategyVeryHigh.java
+++ src/InsuranceStrategyVeryHigh.java
@@ -1,22 +1,21 @@
-public class InsuranceStrategyVeryHigh {
+public class InsuranceStrategyVeryHigh extends InsuranceStrategy {
     public InsuranceStrategyVeryHigh() {
     }
 
 
     //Extracted delegate 11:37AM 2/13/2020
 
-    public double calculateInsuranceVeryHigh(double income) {
-        return (income - getAdjustment()) * getWeight() + getConstant();
-    }
-
+    @Override
     public int getConstant() {
         return 105600;
     }
 
+    @Override
     public double getWeight() {
         return 0.02;
     }
 
+    @Override
     public int getAdjustment() {
         return 60000;
     }
